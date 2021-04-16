c961c24830a8662c3f63c33a1d787824ec2cc874
vuk <vuk>
2021-04-16 13:34:25 -0500

    Worked on tangent line , adjust function

--- tangent.js
+++ tangent.js
@@ -2,57 +2,55 @@ const board = createBoard();
 var q = 0;
 board.suspendUpdate();
 
-var f = board.jc.snippet("sin(x)", true, "x", false);
+
+
+
+var f = board.jc.snippet('function(x) { return x * x * x - 4 * x * x + 4 * x + 16; }', false, "x", false);
+
 var qSliderGraph = board.create(
-  "slider",
-  [
-    [1, 4],
-    [5, 4],
-    [0, 0, 5],
-  ],
-  { visible: false }
+    "slider", [
+        [1, 4],
+        [5, 4],
+        [0, 0, 5],
+    ], { visible: false }
 );
 var plot = createPlot(f);
 
 var glider = board.create("glider", [0, 0, plot], { size: 5, name: "" });
 
 var tanPoint = board.create(
-  "point",
-  [
-    function () {
-      return glider.X() + 0.0001;
-    },
-    function () {
-      return f(glider.X() + 0.0001);
-    },
-  ],
-  { size: 1, name: "p" }
+    "point", [
+        function() {
+            return glider.X() + 0.0001;
+        },
+        function() {
+            return f(glider.X() + 0.0001);
+        },
+    ], { size: 1, name: "p" }
 );
 
 var tanPoint2 = board.create(
-  "point",
-  [
-    function () {
-      return glider.X() + qSliderGraph.Value();
-    },
-    function () {
-      return f(glider.X() + qSliderGraph.Value());
-    },
-  ],
-  { size: 3, name: "q" }
+    "point", [
+        function() {
+            return glider.X() + qSliderGraph.Value();
+        },
+        function() {
+            return f(glider.X() + qSliderGraph.Value());
+        },
+    ], { size: 3, name: "q" }
 );
 
 var line = board.create("line", [glider, tanPoint], {
-  strokeColor: "#ff0000",
-  dash: 2,
+    strokeColor: "#ff0000",
+    dash: 2,
 });
 
 var line2 = board.create("line", [glider, tanPoint2], {
-  strokeColor: "#0000ff",
-  dash: 2,
+    strokeColor: "#0000ff",
+    dash: 2,
 });
 board.unsuspendUpdate();
 
 $(document).ready(() => {
-  sliderChanger("q", "qVal", q, qSliderGraph, board);
+    sliderChanger("q", "qVal", q, qSliderGraph, board);
 });
--- univeraslFunc.js
+++ univeraslFunc.js
@@ -1,54 +1,54 @@
 const createBoard = () => {
-  return JXG.JSXGraph.initBoard("jxgbox", {
-    axis: true,
-    boundingbox: [-10, 7, 10, -7],
-    showCopyright: false,
-    pan: {
-      enabled: true,
-      needShift: false,
-    },
-    zoom: {
-      factorX: 1.25,
-      factorY: 1.25,
-      wheel: true,
-      needShift: true,
-      min: 0.001,
-      max: 1000.0,
-      pinchHorizontal: true,
-      pinchVertical: true,
-      pinchSensitivity: 7,
-    },
-  });
+    return JXG.JSXGraph.initBoard("jxgbox", {
+        axis: true,
+        boundingbox: [-10, 27, 10, -7],
+        showCopyright: false,
+        pan: {
+            enabled: true,
+            needShift: false,
+        },
+        zoom: {
+            factorX: 1.25,
+            factorY: 1.25,
+            wheel: true,
+            needShift: true,
+            min: 0.001,
+            max: 1000.0,
+            pinchHorizontal: true,
+            pinchVertical: true,
+            pinchSensitivity: 7,
+        },
+    });
 };
 const createPlot = (f) => {
-  return board.create("functiongraph", [f], {
-    strokeColor: "black",
-    strokeWidth: 2,
-  });
+    return board.create("functiongraph", [f], {
+        strokeColor: "black",
+        strokeWidth: 2,
+    });
 };
 
-$(window).resize(function () {
-  if ($(this).width() <= 1000) {
-    $("#jxgbox").css("height", "60vh");
-  } else $("#jxgbox").css("height", "90vh");
-  $(".JXG_navigation_button").toArray()[1].click();
+$(window).resize(function() {
+    if ($(this).width() <= 1000) {
+        $("#jxgbox").css("height", "60vh");
+    } else $("#jxgbox").css("height", "90vh");
+    $(".JXG_navigation_button").toArray()[1].click();
 });
 
 $(document).keydown((e) => {
-  switch (e.key) {
-    case "ArrowLeft":
-      $(".JXG_navigation_button").toArray()[3].click();
-      break;
-    case "ArrowRight":
-      $(".JXG_navigation_button").toArray()[6].click();
-      break;
-    case "ArrowUp":
-      $(".JXG_navigation_button").toArray()[5].click();
-      break;
-    case "ArrowDown":
-      $(".JXG_navigation_button").toArray()[4].click();
-      break;
-  }
+    switch (e.key) {
+        case "ArrowLeft":
+            $(".JXG_navigation_button").toArray()[3].click();
+            break;
+        case "ArrowRight":
+            $(".JXG_navigation_button").toArray()[6].click();
+            break;
+        case "ArrowUp":
+            $(".JXG_navigation_button").toArray()[5].click();
+            break;
+        case "ArrowDown":
+            $(".JXG_navigation_button").toArray()[4].click();
+            break;
+    }
 });
 // var nSlider = document.getElementById("n");
 // var noutput = document.getElementById("nVal");
@@ -62,15 +62,15 @@ $(document).keydown((e) => {
 // };
 
 const sliderChanger = (sliderId, displayId, tracker, graphSlider, board, param) => {
-  var slider = document.getElementById(sliderId);
-  var output = document.getElementById(displayId);
-  output.innerHTML = slider.value;
-  tracker = graphSlider.Value();
-  slider.value = tracker;
-  slider.oninput = function () {
-    output.innerHTML = this.value;
-    graphSlider.setValue(this.value);
-    board.fullUpdate();
-    param && param();
-  };
+    var slider = document.getElementById(sliderId);
+    var output = document.getElementById(displayId);
+    output.innerHTML = slider.value;
+    tracker = graphSlider.Value();
+    slider.value = tracker;
+    slider.oninput = function() {
+        output.innerHTML = this.value;
+        graphSlider.setValue(this.value);
+        board.fullUpdate();
+        param && param();
+    };
 };

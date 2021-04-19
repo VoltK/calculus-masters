573c8059b0b6e382d483efe32644cb6eecf31ddd
Prabin Regmi <iprabin@Prabins-MacBook-Pro.local>
2021-04-19 14:18:43 -0400

    added the blank points and left hand line for sec

--- Limits.js
+++ Limits.js
@@ -1,7 +1,9 @@
 var eq = ["-x^3-4*x^2+4*x+16", "tan(x)", "x+5"];
-var ef = 0;
-var piece;
-var l = 0;
+var ef = 0,
+  l = 0,
+  piece,
+  piecePoint1,
+  piecePoint2;
 
 const board = createBoard();
 
@@ -18,6 +20,8 @@ const updateGraph = () => {
     pieceWise();
   } else {
     board.removeObject(piece);
+    board.removeObject(piecePoint1);
+    board.removeObject(piecePoint2);
     plot = createPlot(f);
     $("#math-display2").css("display", "none");
     $("#math-display3").css("display", "none");
@@ -30,17 +34,23 @@ const limit = () => {
   board.removeObject(point);
   if (ef > 1) {
     var y = 0;
-    if (Number(l) >= 2) y = plot.Y(Number(l));
-    else if (Number(l) < 2) y = piece.Y(Number(l));
+    if (Number(l) > 2) y = plot.Y(Number(l));
+    else if (Number(l) <= 2) y = piece.Y(Number(l));
     point = board.create("point", [Number(l), y], {
       name: "",
       fixed: true,
+      fillColor: "#170A1C",
+      strokeColor: "#170A1C",
+      size: 5,
     });
     $("#limit").text(` Limit = ${Number(l) == 2 ? "DNE" : y}`);
   } else {
     point = board.create("point", [Number(l), plot.Y(Number(l))], {
       name: "",
       fixed: true,
+      fillColor: "#170A1C",
+      strokeColor: "#170A1C",
+      size: 5,
     });
     $("#limit").text(` Limit = ${plot.Y(Number(l)) == Infinity ? "DNE" : plot.Y(Number(l))}`);
   }
@@ -88,8 +98,8 @@ const pieceWise = () => {
       99999,
     ],
     {
-      strokeColor: "black",
-      strokeWidth: 2,
+      strokeColor: "#D36582",
+      strokeWidth: 3,
     }
   );
   piece = board.create(
@@ -98,14 +108,27 @@ const pieceWise = () => {
       function (x) {
         return -(1 / 2) * x + 3;
       },
-      1,
+      2,
       -99999,
     ],
     {
-      strokeColor: "red",
-      strokeWidth: 2,
+      strokeColor: "#D36582",
+      strokeWidth: 3,
     }
   );
+  piecePoint1 = board.create("point", [2, 7], {
+    name: "",
+    fixed: true,
+    fillColor: "#07A0C3",
+    strokeColor: "#07A0C3",
+  });
+  piecePoint2 = board.create("point", [2, 2], {
+    name: "",
+    fixed: true,
+    showInfobox: false,
+    fillColor: "#FFFFFC",
+    strokeColor: "#07A0C3",
+  });
   $("#math-display1").css("display", "none");
   $("#math-display2").css("display", "block");
   $("#math-display3").css("display", "block");
--- tangent.js
+++ tangent.js
@@ -1,56 +1,100 @@
 const board = createBoard();
-var q = 0;
+var q1,
+  q2 = 0;
 board.suspendUpdate();
 
-
-
-
-var f = board.jc.snippet('function(x) { return x * x * x - 4 * x * x + 4 * x + 16; }', false, "x", false);
-
+var f = board.jc.snippet("x^3-4*x^2+4*x+16", true, "x", false);
 var qSliderGraph = board.create(
-    "slider", [
-        [1, 4],
-        [5, 4],
-        [0, 0, 5],
-    ], { visible: false }
+  "slider",
+  [
+    [1, 4],
+    [5, 4],
+    [0, 0, 5],
+  ],
+  { visible: false }
+);
+var q2SliderGraph = board.create(
+  "slider",
+  [
+    [1, 5],
+    [5, 5],
+    [-5, 0, 0],
+  ],
+  { visible: false }
 );
 var plot = createPlot(f);
 
-var glider = board.create("glider", [0, 0, plot], { size: 5, name: "" });
+var glider = board.create("glider", [0, 0, plot], {
+  size: 5,
+  name: "",
+  strokeColor: "#662E9B",
+  fillColor: "#662E9B",
+});
 
 var tanPoint = board.create(
-    "point", [
-        function() {
-            return glider.X() + 0.0001;
-        },
-        function() {
-            return f(glider.X() + 0.0001);
-        },
-    ], { size: 1, name: "p" }
+  "point",
+  [
+    function () {
+      return glider.X() + 0.0001;
+    },
+    function () {
+      return f(glider.X() + 0.0001);
+    },
+  ],
+  { size: 1, name: "p", size: 6 }
 );
 
 var tanPoint2 = board.create(
-    "point", [
-        function() {
-            return glider.X() + qSliderGraph.Value();
-        },
-        function() {
-            return f(glider.X() + qSliderGraph.Value());
-        },
-    ], { size: 3, name: "q" }
+  "point",
+  [
+    function () {
+      return glider.X() + qSliderGraph.Value();
+    },
+    function () {
+      return f(glider.X() + qSliderGraph.Value());
+    },
+  ],
+  {
+    size: 2,
+    name: "q1",
+  }
 );
 
+var tanPoint3 = board.create(
+  "point",
+  [
+    function () {
+      return glider.X() + q2SliderGraph.Value();
+    },
+    function () {
+      return f(glider.X() + q2SliderGraph.Value());
+    },
+  ],
+  {
+    size: 2,
+    name: "q2",
+  }
+);
 var line = board.create("line", [glider, tanPoint], {
-    strokeColor: "#ff0000",
-    dash: 2,
+  strokeColor: "#EE964B",
+  dash: 2,
+  strokeWidth: 3,
 });
 
 var line2 = board.create("line", [glider, tanPoint2], {
-    strokeColor: "#0000ff",
-    dash: 2,
+  strokeColor: "#63C7B2",
+  dash: 2,
+  strokeWidth: 3,
+});
+
+var line3 = board.create("line", [glider, tanPoint3], {
+  strokeColor: "#EF9CDA",
+  dash: 2,
+  strokeWidth: 3,
 });
 board.unsuspendUpdate();
 
 $(document).ready(() => {
-    sliderChanger("q", "qVal", q, qSliderGraph, board);
+  sliderChanger("q", "qVal", q1, qSliderGraph, board);
+  sliderChanger("q2", "q2Val", q2, q2SliderGraph, board);
 });
